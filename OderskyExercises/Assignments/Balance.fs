6f47b5d7d5ee5cd8ecdf4213a1ab7f90699ccc7c
Paolo del Mundo <paolodm@gmail.com>
2013-03-26 22:35:08 -0400

    New algorithm implementation:
    1. I count the number of parenthesis.
    2. If the closing parens ever exceeds the number of open parens, it automatically fails.
    3. If we go through the whole input, we only pass if there are equal number of open and close parens (must be 0).
    
    This is working, but I need to make it simpler.

--- OderskyExercises/Assignments/Balance.fs
+++ OderskyExercises/Assignments/Balance.fs
@@ -3,25 +3,24 @@ module Assignment2
     open System
 
     let balance (chars:string) = 
-        let rec calc (remaining:string) =
-            let start_ind = remaining.IndexOf("(")
-            let end_ind = remaining.LastIndexOf(")")
+        let rec calc (remaining:string) (acc:int) =
+            if remaining.Length > 0 then
+                let first = remaining.[0]
+                let left = remaining.Substring(1)
             
-            //Console.WriteLine("Start: {0}", start_ind)
-            //Console.WriteLine("End: {0}", end_ind)
-            Console.WriteLine("Remaining: {0}", remaining)
-
-
-            if start_ind > end_ind then
-                false
-            elif start_ind = -1 then
-                end_ind = -1
-            elif end_ind = -1 then
-                start_ind = -1
+                if acc < 0 then
+                    acc 
+                elif first = '(' then
+                    calc left (acc + 1)
+                elif first = ')' then
+                    calc left (acc - 1)
+                else
+                    calc left acc
             else
-                let substr = remaining.Substring(start_ind + 1, (end_ind - start_ind - 1))
-                calc substr
-        calc chars 
+                acc
+        let acc = 0
+        let result = calc chars acc
+        result = 0
 
     let ans1 = balance "(if (zero? x) max (/ 1 x))"
     let ans2 = balance "I told him (that it’s not (yet) done). (But he wasn’t listening)"
