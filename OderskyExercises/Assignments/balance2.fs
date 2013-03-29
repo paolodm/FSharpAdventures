3eace7c2bd5835c79b2e3157299ede0cfdc5dcb2
Paolo del Mundo <paolodm@gmail.com>
2013-03-28 23:16:18 -0400

    In change.fs, I used a .NET generic List instead of the F# list object. It is correctly reporting the number of possible sequences now, but I'm cheating a bit by using a mutable data structure (using the mutable generic List). I will need to change this so it is more consistent with the immutable paradigm.

---
+++ OderskyExercises/Assignments/balance2.fs
@@ -0,0 +1,24 @@
+﻿module balance2
+
+    let balance (input:string) = 
+
+        // parens_ints is an array that represents the parens. 
+        //      1 stands for '('. 
+        //      -1 stands for ')'.
+        // I have removed all the non-parenthesis from this array.
+        let parens_ints = 
+            input.ToCharArray()
+                |> List.map(fun x -> if x = '(' then 1 
+                                    elif x = ')' then -1
+                                    else 0)
+                |> Seq.filter(fun x -> x <> 0)
+
+        let rec calc (parens_ints:List<int>) count cur_ind = 
+            if count < 0 then
+                count
+            elif cur_ind > parens_ints.Length then
+                count
+            else
+                calc parens_ints (count + parens_ints.[cur_ind]) (cur_ind + 1)
+    
+        calc parens_ints 0 0
---
+++ OderskyExercises/Assignments/change.fs
@@ -0,0 +1,31 @@
+﻿module change
+
+    open System
+    open System.Collections.Generic
+
+    let countChange (amount:int) (coins:list<'a>):int = 
+        let breakdowns = new List<list<'a>>()
+
+        let add_to_breakdowns breakdown = 
+            let sorted = List.sort breakdown
+            let alreadyAdded = breakdowns.Contains(sorted)
+            
+            if not alreadyAdded then
+                breakdowns.Add(sorted)
+                // List.append breakdowns sorted 
+                Console.WriteLine(sorted)
+
+        let rec loop remaining breakdown = 
+            if remaining = 0 then
+                add_to_breakdowns breakdown
+            elif remaining > 0 then
+                coins
+                    |> Seq.filter(fun x -> remaining >= x)
+                    |> Seq.iter(fun x -> loop (remaining - x) (x::breakdown))
+                    |> ignore
+
+        loop amount []
+
+        breakdowns.Count
+
+    let ans = countChange 4 [1; 2];
